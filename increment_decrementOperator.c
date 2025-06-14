d61c369af11418bacf47ebbba5f3eba54c6006ca
Maheshlab <sthamahesh600@gmail.com>
2025-06-14 10:50:53 +0545

    lala

---
+++ increment_decrementOperator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int a = 5000;
+int b = 6000;
+printf("Mahesh Kumar Shrestha\n");
+printf("a=%d,b=%d\n",a,b);
+b=++a;
+printf("a=%d, b=%d\n",a,b);
+b=a++;
+printf("a=%d, b=%d\n",a,b); 
+b=a--;
+printf("a=%d, b=%d\n",a,b); 
+b=--a;
+printf("a=%d, b=%d\n",a,b);
+return 0;
+}
