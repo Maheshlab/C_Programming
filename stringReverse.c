d6bb17bf131d2ffec485e2b1f5dcdef380578248
Maheshlab <sthamahesh600@gmail.com>
2025-06-18 08:42:23 +0545

    String Reverse

--- stringReverse.c
+++ stringReverse.c
@@ -1,31 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char str[100], temp;
-    int i, j;
-    printf("Mahesh Kumar Shrestha\n");
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-
-    // Remove newline character if present
-    str[strcspn(str, "\n")] = '\0';
-
-    i = 0;
-    j = strlen(str) - 1;
-
-    // Swap characters from start and end
-    while (i < j) {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
-    }
-
-    printf("Reversed string: %s\n", str);
-
-    return 0;
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char string[25];
+    printf("Enter the string you want to reverse:\n");
+    scanf("%s", string);
+    strrev(string);
+    printf("The reversed string is: %s\n", string);
+return 0;
 }
-
-
