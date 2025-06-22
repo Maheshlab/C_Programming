8d42645a545f86cf97b58164b2994c5e9f0a016b
Maheshlab <sthamahesh600@gmail.com>
2025-06-22 08:59:14 +0545

    Palindrome

--- palindrome.c
+++ palindrome.c
@@ -11,7 +11,7 @@ int main() {
     original = num;  // Store original number for comparison
 
     // Reverse the number
-    while (num != 0) {
+    while (num != 0) { 
         remainder = num % 10;
         reversed = reversed * 10 + remainder;
         num = num / 10;
