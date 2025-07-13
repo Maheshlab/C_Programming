117f09e7036d6c73b70a2836d104baebf0718f57
Maheshlab <sthamahesh600@gmail.com>
2025-07-13 18:24:05 +0545

    delete

--- stringCombine.c
+++ stringCombine.c
@@ -1,8 +1,6 @@
 //Program to combine the two strings
 #include <stdio.h>    
 #include <string.h>   
-#include <conio.h>    
-
 int main()            
 {
     char string1[30], string2[30];
@@ -17,7 +15,6 @@ int main()
     
     value = strcat(string1, string2);    
     printf("The combined string is: %s\n", value); 
-    
-    getch();      
+          
     return 0;        
 }
