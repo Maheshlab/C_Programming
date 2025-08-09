e13b6f1e2e5f8bd809a2e318ca47550042d5c323
Maheshlab <sthamahesh600@gmail.com>
2025-08-09 17:26:24 +0545

    String Reverse

--- string_reverse.c
+++ string_reverse.c
@@ -1,12 +1,17 @@
-#include<stdio.h>
-#include<string.h>
-int main()
+// Program to reverse the string
+#include <stdio.h>
+#include <string.h>
+void main()
 {
-    char character[25];
-    printf("Mahesh Kumar Shrestha\n");
-    printf("Enter the character you want to reverse:\n");
-    scanf("%s", character);
-    strrev(character);
-    printf("The reversed character is: %s\n", character);
-return 0;
+char string[25], reverse_string[25];
+int length, i, j;
+printf("Mahesh Kumar Shrestha\n");
+printf("Input string to be reversed:\n");
+scanf("%s", string);
+length=strlen(string);
+for(j=0,i=length-1;j<length;j++,i--)
+reverse_string[j]=string[i];
+reverse_string[j]='\0';
+puts(reverse_string);
+
 }
