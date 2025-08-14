cb23ad9ec8bd1a6202fb5fa20e6efbf8e1199dec
Maheshlab <sthamahesh600@gmail.com>
2025-08-14 08:32:50 +0545

    Finding the max and min values

--- find_min_max.c
+++ find_min_max.c
@@ -1,25 +1,26 @@
+// Program that takes 10 integer value and finds the maximum and minimum value
 #include <stdio.h>
 
 int main() {
-    int numbers[10];
+    int num[10];
     int i, max, min;
 
     // Input 10 numbers
     printf("Enter 10 integer values:\n");
     for(i = 0; i < 10; i++) {
         printf("Enter number %d: ", i + 1);
-        scanf("%d", &numbers[i]);
+        scanf("%d", &num[i]);
     }
 
     // Initialize max and min to the first element
-    max = min = numbers[0];
+    max = min = num[0];
 
     // Find max and min
     for(i = 1; i < 10; i++) {
-        if(numbers[i] > max)
-            max = numbers[i];
-        if(numbers[i] < min)
-            min = numbers[i];
+        if(num[i] > max)
+            max = num[i];
+        if(num[i] < min)
+            min = num[i];
     }
 
     // Output results
