d24f8b307e2252a02b6da9abcb715db42a4ab01c
Maheshlab <sthamahesh600@gmail.com>
2025-08-14 08:34:30 +0545

    Use of while loop

---
+++ while_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//Mahesh Kumar Shrestha
+// int main() {
+//     int i = 2;
+//     printf("Even numbers up to hundred:\n");
+//     //Using while loop
+//     while (i <= 100) {
+//         printf("%d ", i);
+//         i += 2;
+//     }
+//     return 0;
+// }
+
+
+int main() {
+    int i = 2;
+    printf("Even numbers up to hundred:\n");
+    //Using do-while loop
+    do {
+        printf("%d ", i);
+        i += 2;
+    } while (i <= 100);
+    return 0;
+}
