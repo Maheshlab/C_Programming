610c8bd668430f4d0863df37faad89f7a370e909
Maheshlab <sthamahesh600@gmail.com>
2025-08-20 20:21:38 +0545

    Pointer

--- pointer.c
+++ pointer.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-int main () {
-    int x=20, *p=&x;
-    printf("The address of x is : %u\n", &x);
-    printf("The address of x (value of p or address pointed by p) is : %u\n", p);
-    printf("The value of x is : %d\n", x);
-    printf("The value of x (value pointed by p) is : %u\n", *p);
-    printf("The address of p is :%u\n", &p);
-    return 0;
+// This program demonstrates the use of pointers in C by taking an integer input
+#include <stdio.h>
 
+int main() {
+    int num;        // Normal integer variable
+    int *ptr;       // Pointer to an integer
+
+    ptr = &num;     // Point ptr to the address of num
+
+    printf("Enter a number:\n ");
+    scanf("%d", ptr);   // Taking input using pointer
+
+    printf("You entered: %d\n", *ptr);         // Dereferencing to print value
+    printf("Stored at address: %p\n", ptr);      // Pointer holds address of num
+
+    return 0;
 }
