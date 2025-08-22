c4d63c02ecdfa07fc251c8a66ac231021cc1c178
Maheshlab <sthamahesh600@gmail.com>
2025-08-22 23:50:20 +0545

    Matrix sum of lower triangle

---
+++ sum_of_lower_traingle.c
@@ -0,0 +1,35 @@
+// C program to calculate the sum of element of lower triangle of 3X3 matrix
+#include<stdio.h>
+int main() 
+{
+int mat[3][3], i,j,sum=0;
+printf("Mahesh Kumar Shrestha\n");
+printf("Enter the elements of matrix:\n");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+
+scanf("%d",&mat[i][j]);
+
+}
+
+}
+printf("Sum of element of lower triangle is:\n");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+
+if(i>=j)
+{
+
+sum=sum+mat[i][j];
+
+}
+
+}
+}
+printf("%d",sum);
+return 0;
+}
