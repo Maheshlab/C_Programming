4f30908b1f6c392e1c4038d8ae70a3d037d7d221
Maheshlab <sthamahesh600@gmail.com>
2025-08-26 11:41:22 +0545

    String length

---
+++ string_length.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#include<string.h>
+int main()
+{
+	int length=0;
+	char string2[20];
+	printf("Mahesh Kumar Shrestha\n");
+	printf("Enter the string\n");
+	scanf("%s", string2);
+	length=strlen(string2);
+	printf("The length of string is %d\n",length);
+	return 0;
+}
